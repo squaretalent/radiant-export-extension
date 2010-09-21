07f625f2f2e8f8913c906f3e82aa69ea88d3c906
Dirk Kelly <dk+summer@dirkkelly.com>
2010-09-21 11:02:15 +0800

    fixed a segfaul being caused by a dodgy extension definition file

--- export_extension.rb
+++ export_extension.rb
@@ -4,12 +4,4 @@ class ExportExtension < Radiant::Extension
   version "0.1"
   description "An extension version of yaml_db which exports schemas in order to support extension migrations"
   url "http://github.com/squaretalent/radiant-export-extension"
-
-  def activate
-    # Models
-    User.send :include, Scoped::Models::User
-    
-    # Controllers
-    ApplicationController.send :include, Scoped::Lib::LoginSystem
-  end
 end
